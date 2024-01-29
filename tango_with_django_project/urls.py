2c562d29be8cd31e3717bd1c2c7c33d2e2089413
Jad Tarabay <jadtarabay@glaroam2-132-120.wireless.gla.ac.uk>
2024-01-29 12:39:49 +0000

    Second commit

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from django.urls import include
+from rango import views 
+
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    # The above maps any URLs starting with rango/ to be handled by rango
     path('admin/', admin.site.urls),
-]
+    path('about/', views.about, name='about'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
