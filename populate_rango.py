d89cbfdefb4b91961a8b424bdc04743581c597c9
Jad Tarabay <jadtarabay@Jads-MacBook-Pro.local>
2024-02-05 15:00:17 +0000

    Chapter 8

--- populate_rango.py
+++ populate_rango.py
@@ -66,7 +66,7 @@ def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
     c.likes = likes
-    c.save()
+    c.save()    
     return c
 
 if __name__ == '__main__':
